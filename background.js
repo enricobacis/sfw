37d986aed747553e4e67fd4024966fb524573d53
enricobacis <enrico.bacis@gmail.com>
2016-04-10 01:00:50 +0200

    Remove the page from history if it gets there. Also remove the sfw page from history (it leaves traces)

--- background.js
+++ background.js
@@ -1,21 +1,27 @@
 ﻿var prohibited = ['porn'];
 var sfw = 'https://www.youtube.com/embed/gCk4YAA-6PM?start=5&autoplay=1'
 
+chrome.history.onVisited.addListener(function (result) {
+    if (result.url == sfw || is_prohibited(result.url)) {
+      chrome.history.deleteUrl({ url: result.url });
+    }
+});
+
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
-    var found = false;
-    prohibited.forEach(function (x) {
-        if (details.url.includes(x)) {
-            found = true;
-        }
-    });
-    if (found) {
-        return {
-            redirectUrl: sfw
-        };
+    if (is_prohibited(details.url)) {
+      return { redirectUrl: sfw };
     }
   },
-  {
-    urls: ["<all_urls>"]
-  },
+  { urls: ["<all_urls>"] },
   ['blocking']
 );
+
+function is_prohibited(url) {
+  var found = false;
+  prohibited.forEach(function (x) {
+      if (url.includes(x)) {
+        found = true;
+      }
+  });
+  return found;
+}
