be5b277f37b6807c2c2396eb950680ce675f1f1b
enricobacis <enrico.bacis@gmail.com>
2016-04-09 17:00:35 +0200

    First version, blocks porn

---
+++ background.js
@@ -0,0 +1,21 @@
+﻿var prohibited = ['porn'];
+var sfw = 'https://www.youtube.com/embed/gCk4YAA-6PM?start=5&autoplay=1'
+
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+    var found = false;
+    prohibited.forEach(function (x) {
+        if (details.url.includes(x)) {
+            found = true;
+        }
+    });
+    if (found) {
+        return {
+            redirectUrl: sfw
+        };
+    }
+  },
+  {
+    urls: ["<all_urls>"]
+  },
+  ['blocking']
+);
